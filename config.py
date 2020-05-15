4e5b70d0822e2a36c6ee18fdda184db92c2229a1
ShriramShagri <shagrisrm@gmail.com>
2020-05-15 23:15:36 +0530

    Test

---
+++ config.py
@@ -0,0 +1,7 @@
+def cred():
+    redis = {
+        'host' : "redis-17770.c11.us-east-1-2.ec2.cloud.redislabs.com", 
+        'port' : '17770', 
+        'password' : 'tdil1lcm38eQIOKi3hYdjBsmXIu79TmH'
+    }
+    return redis
